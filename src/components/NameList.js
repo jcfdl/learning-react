551bf603e5981b1e8d79c76b51d5c288e9345960
jcfdl <jc.deleon027@gmail.com>
2020-04-06 10:51:20 +0800

    Index as Key

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,24 +2,27 @@ import React from 'react';
 import PersonList from './PersonList'
 
 function NameList() {
-	// const names = ['Jc', 'Josh', 'mae']
-	const persons = [
-		{
-			id: 1,
-			name: 'JC',
-			age: 23,
-			skill: 'All'
-		}, 
-		{
-			id: 2,
-			name: 'Joshua',
-			age: 21,
-			skill: 'None'
-		}
-	]			
+	const names = ['Jc', 'Josh', 'mae']
+	// const persons = [
+	// 	{
+	// 		id: 1,
+	// 		name: 'JC',
+	// 		age: 23,
+	// 		skill: 'All'
+	// 	}, 
+	// 	{
+	// 		id: 2,
+	// 		name: 'Joshua',
+	// 		age: 21,
+	// 		skill: 'None'
+	// 	}
+	// ]			
 
-	const personList = persons.map(person => <PersonList person={person} />)
-	return <div>{personList}</div>
+	// const personList = persons.map(person => <PersonList key={person.id} person={person} />)
+
+	const nameList = names.map((name, index) => <h2 key={index}>{name}</h2>)
+
+	return <div>{nameList}</div>
 }
 
 export default NameList;
